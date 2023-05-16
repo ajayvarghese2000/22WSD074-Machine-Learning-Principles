282605a073e29566d9f0a03c927cac40740f608c
ajayvarghese2000 <58085441+ajayvarghese2000@users.noreply.github.com>
2023-05-16 16:27:22 +0100

    DAy

---
+++ 4 ML Example/test.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+
+df = pd.DataFrame({'animal':['snake', 'bat', 'tiger', 'lion',
+                   'fox', 'eagle', 'shark', 'dog', 'deer']})
+print(df.head())
